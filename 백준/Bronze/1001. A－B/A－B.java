bb5af395646f636f6c2e2b151b12b5d39c674d5e
lim1t <namest504@gmail.com>
2023-10-05 01:52:45 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        bw.write(String.valueOf(a-b));
+        bw.flush();
+    }
+}
