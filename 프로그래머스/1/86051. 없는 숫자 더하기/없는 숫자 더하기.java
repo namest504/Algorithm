fcd6c3ec65dcc1eef4e33b2f88415c515f933aa1
lim1t <namest504@gmail.com>
2023-11-22 23:06:00 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for(int n : numbers){
+            answer-=n;
+        }
+        return answer;
+    }
+}
