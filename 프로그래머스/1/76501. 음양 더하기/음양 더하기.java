d076f1a2cbe6761dd4b6f742d25201707fb5b289
lim1t <namest504@gmail.com>
2023-11-28 21:39:56 +0900

    [level 1] Title: 음양 더하기, Time: 0.05 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for(int i = 0; i < signs.length; i++){
+            answer += absolutes[i] * (signs[i] ? 1 : -1);
+        }
+        return answer;
+    }
+}
