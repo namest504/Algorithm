aa1c3fa76b27ad63fcdb12a391caf85a47d3a22e
lim1t <namest504@gmail.com>
2023-12-16 23:46:18 +0900

    [level 1] Title: 예산, Time: 0.32 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        
+        for(int i : d){
+            if((budget - i) >= 0){
+                budget -= i;
+                answer += 1;
+            }
+            else break;
+        }
+        return answer;
+    }
+}
