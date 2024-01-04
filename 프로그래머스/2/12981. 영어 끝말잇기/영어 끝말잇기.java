c96e75eeecfc9010d72bf353f8f9bc04c5287d8e
lim1t <namest504@gmail.com>
2024-01-04 15:47:54 +0900

    [level 2] Title: 영어 끝말잇기, Time: 19.41 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,31 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = {};
-        HashMap<Integer, Integer> person = new HashMap<>();
-        HashMap<String, Boolean> record = new HashMap<>();
-        int turn = 0;
-        int currentPerson;
-        String prevWord = "";
+        HashMap<Integer, Integer> person = new HashMap<>(); // 참여자의 차례가 몇 번째 인지 기록
+        HashMap<String, Boolean> record = new HashMap<>(); // 끝말잇기에 사용된 단어 기록
+        
+        int turn = 0; // 끝말잇기 진행 턴 기록
+        int currentPerson; // 현재 참여자의 번호를 계산하고 저장할 변수
+        String prevWord = ""; // 이전 끝말잇기 단어 저장할 변수
+        
         for(String word : words){
             turn++;
             currentPerson = getCurrentPerson(n, turn);
             person.put(currentPerson , person.getOrDefault(currentPerson, 0) + 1);
             
-            // System.out.println(current_person + "번 째 "+ person.get(current_person) + "회 : " + word);
+            // 첫 번째 턴이 아니라면 이전 단어의 끝과 현재 단어의 시작을 비교
             if(turn > 1){
-                System.out.println("이전: " + prevWord + " 현재: " + word);
                 if(!isMatchLastWord(prevWord, word)){
-                    System.out.println("끝말잇기 실패");
                     return new int[]{currentPerson, person.get(currentPerson)};
                 }
             }
             
+            // record에 단어가 없다면 record에 저장하고 prevWord에 단어 저장
             if(!record.getOrDefault(word, false)){
-                System.out.println("입력된 단어 " + word);
                 record.put(word, true);
                 prevWord = word;
-            } else{
+            } else{ // 만약 record에 단어가 존재한다면 종료
                 return new int[]{ currentPerson, person.get(currentPerson)};
             }
         }
