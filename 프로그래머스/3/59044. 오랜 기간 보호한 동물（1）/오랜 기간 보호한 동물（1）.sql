1b9fa6a23986eff358df1178a215331dc22a7b29
lim1t <namest504@gmail.com>
2024-02-24 00:39:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,17 @@
+SELECT
+    I.NAME,
+    I.DATETIME
+FROM
+    ANIMAL_INS AS I
+WHERE
+    I.ANIMAL_ID 
+    NOT IN (
+        SELECT 
+            O.ANIMAL_ID
+        FROM 
+            ANIMAL_OUTS AS O
+    )
+ORDER BY
+    I.DATETIME
+LIMIT
+3
