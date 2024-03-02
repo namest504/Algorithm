463c56f29432b23c777c0e7448fcdf549a42cd85
lim1t <namest504@gmail.com>
2024-03-02 11:35:16 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,44 @@
 import java.util.*;
 class Solution {
+    private int[] parent;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visit = new boolean[n];
+        parent = new int[n];
         
-        Queue<Integer> que = new LinkedList<>();
-        for(int j = 0; j < computers.length; j++){
-            if(!visit[j]){
-                answer++;
-                que.offer(j);
-                visit[j] = true;
-                while(!que.isEmpty()){
-                    int now = que.poll();
-                    for(int i = 0; i < computers[now].length; i++){
-                        if(i != now && computers[now][i] == 1 && !visit[i]){
-                            que.offer(i);
-                            visit[i] = true;
-                        }
-                    }
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j && computers[i][j] == 1){
+                    union(i, j);
                 }
             }
         }
-        return answer;
+        
+        Set<Integer> result = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            result.add(find(i));
+        }
+        
+        return result.size();
+    }
+    
+     public void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a < b) {
+            parent[b] = a;
+        } else {
+            parent[a] = b;
+        }
+    }
+    
+    public int find(int a) {
+        if(parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
     }
 }
