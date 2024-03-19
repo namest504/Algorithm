ef4905a5b09aa1ad56bd5ef2631f0191096e2275
lim1t <namest504@gmail.com>
2024-03-19 13:26:21 +0900

    [level 3] Title: 숫자 게임, Time: 660.87 ms, Memory: 18.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -0,0 +1,9 @@
+def solution(A, B):
+    answer = 0
+    A.sort(reverse = True)
+    B.sort(reverse = True)
+    for a in A :
+        if a < B[0] :
+            answer += 1
+            del B[0]
+    return answer
