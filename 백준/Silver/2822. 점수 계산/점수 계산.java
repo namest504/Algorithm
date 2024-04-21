a933ae5e6485241ce03f8e18b5ea76bc5c213a88
lim1t <namest504@gmail.com>
2024-04-21 23:03:12 +0900

    [Silver V] Title: 점수 계산, Time: 224 ms, Memory: 18388 KB -BaekjoonHub

---
+++ 백준/Silver/2822. 점수 계산/점수 계산.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Main {    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int [8][2]; 
+        int[] result = new int [5]; 
+        int sum = 0;
+        for(int i = 0; i < 8; i++){
+            arr[i][0] = sc.nextInt();
+            arr[i][1] = i + 1;
+        }
+        Arrays.sort(arr, (int[] a, int[] b) -> {
+            return b[0] - a[0];           
+        });
+        
+        for(int i = 0; i < 5; i++){
+            sum += arr[i][0];
+            result[i] = arr[i][1];
+        }
+        
+        Arrays.sort(result);
+   
+        System.out.println(sum);
+        
+        for(int i = 0; i < 5; i++){
+            System.out.print(result[i] + " ");        
+        }      
+        
+        sc.close();
+    }
+}
