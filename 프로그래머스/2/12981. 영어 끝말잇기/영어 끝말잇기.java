5119aa301b4f3f9788cc3912d6bc02df795c4e66
lim1t <namest504@gmail.com>
2024-10-03 18:41:49 +0900

    [level 2] Title: 영어 끝말잇기, Time: 17.08 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,43 +1,43 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int n, String[] words) {
-        HashMap<Integer, Integer> person = new HashMap<>(); // 참여자의 차례가 몇 번째 인지 기록
-        HashMap<String, Boolean> record = new HashMap<>(); // 끝말잇기에 사용된 단어 기록
-        
-        int turn = 0; // 끝말잇기 진행 턴 기록
-        int currentPerson; // 현재 참여자의 번호를 계산하고 저장할 변수
-        String prevWord = ""; // 이전 끝말잇기 단어 저장할 변수
-        
-        for(String word : words){
-            turn++;
-            currentPerson = getCurrentPerson(n, turn);
-            person.put(currentPerson , person.getOrDefault(currentPerson, 0) + 1);
-            
-            // 첫 번째 턴이 아니라면 이전 단어의 끝과 현재 단어의 시작을 비교
-            if(turn > 1){
-                if(!isMatchLastWord(prevWord, word)){
-                    return new int[]{currentPerson, person.get(currentPerson)};
+        int[] answer = new int[]{0, 0};
+
+        Set<String> record = new HashSet<>();
+        int[] count = new int[n];
+        /*
+        n = 3
+        0 1번
+        1 2번
+        2 3번
+        3 1번
+        4 2번
+        5 3번
+        6 1번
+        i%3 + 1
+        */
+        char before = words[0].charAt(words[0].length()-1);
+        char now;
+        for(int i = 0; i < words.length; i++) {
+            int man = i%n + 1; // x 번 째 사람
+            count[man-1] += 1;
+            if(i != 0) {
+                before = words[i-1].charAt(words[i-1].length()-1);
+                now = words[i].charAt(0);
+                if(before != now) {
+                    return new int[]{man, count[man-1]};
                 }
             }
-            
-            // record에 단어가 없다면 record에 저장하고 prevWord에 단어 저장
-            if(!record.getOrDefault(word, false)){
-                record.put(word, true);
-                prevWord = word;
-            } else{ // 만약 record에 단어가 존재한다면 종료
-                return new int[]{ currentPerson, person.get(currentPerson)};
+            System.out.println("before : " + before);
+            if(!record.contains(words[i])) {
+                record.add(words[i]);
+            } else {
+                return new int[]{man, count[man-1]};
             }
+            System.out.println(man + " : " + count[man-1]);
         }
 
-        return new int[]{0, 0};
-    }
-    
-    private int getCurrentPerson(int n, int turn){
-        return turn%n == 0 ? n : turn%n;
-    }
-    
-    private boolean isMatchLastWord(String before, String after){
-         System.out.println(before.split("")[before.length() - 1] + "과 " + after.split("")[0] + "비교");
-        return before.split("")[before.length() - 1].equals(after.split("")[0]);
+        return answer;
     }
 }
